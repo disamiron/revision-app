71232ac5b8470f902919768bfca38e0cf8de3797
disamiron <denis.mironov@ddgcorp.ru>
2022-11-21 22:24:44 +0300

    only useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'top',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
